0213adb9428df6bc0acf25f2552e8df0a86962ef
Artem Kovalev <akovalev@mentalstack.com>
2021-10-18 18:44:39 +0300

    add project

---
+++ KnowledgeTree/KnowledgeTree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KnowledgeTree
+//
+//  Created by Artem Kovalev on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
